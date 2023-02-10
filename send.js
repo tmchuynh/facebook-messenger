53876d0fb911f1223fc1e7180c97a4c43af757c5
Tina Huynh <tinamchuynh@gmail.com>
2023-02-10 11:15:39 -0800

    add profile icons

--- send.js
+++ send.js
@@ -2,6 +2,9 @@ const form = document.querySelector("form");
 const input = document.querySelector("input[type='text']");
 const messageThread = document.querySelector(".message-thread");
 
+const friendsList = document.querySelector(".friends-list");
+const friends = friendsList.querySelectorAll(".friend");
+
 form.addEventListener("submit", e => {
   e.preventDefault();
   const message = input.value;
@@ -23,3 +26,38 @@ input.addEventListener("keydown", e => {
     form.dispatchEvent(new Event("submit"));
   }
 });
+
+friendsList.addEventListener("click", e => {
+  if (!e.target.classList.contains("friend")) return;
+
+  friends.forEach(friend => {
+    friend.classList.remove("selected");
+  });
+  e.target.classList.add("selected");
+
+  // Load the conversation with the selected friend
+  const friendId = e.target.dataset.friendId;
+  loadConversation(friendId);
+});
+
+function loadConversation(friendId) {
+    const messageThread = document.querySelector(".message-thread");
+    messageThread.innerHTML = "";
+  
+    messages
+      .filter(message => message.from === 1 || message.to === 1)
+      .forEach(message => {
+        const messageBubble = document.createElement("div");
+        messageBubble.classList.add("message-bubble");
+        messageBubble.innerHTML = `<p>${message.text}</p>`;
+  
+        if (message.from === 1) {
+          messageBubble.classList.add("sent");
+        }
+  
+        messageThread.appendChild(messageBubble);
+      });
+  
+    messageThread.scrollTop = messageThread.scrollHeight;
+  }
+  
