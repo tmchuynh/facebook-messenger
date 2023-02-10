dedbd90a7d7da9b7f1f8fed60831b7fe9fd3d16f
Tina Huynh <tinamchuynh@gmail.com>
2023-02-10 11:03:49 -0800

    send message on enter

--- send.js
+++ send.js
@@ -1,1 +1,25 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
+const input = document.querySelector("input[type='text']");
+const messageThread = document.querySelector(".message-thread");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = input.value;
+
+  if (!message) return;
+
+  const messageBubble = document.createElement("div");
+  messageBubble.classList.add("message-bubble");
+  messageBubble.classList.add("sent");
+  messageBubble.innerHTML = `<p>${message}</p>`;
+
+  messageThread.appendChild(messageBubble);
+  input.value = "";
+  messageThread.scrollTop = messageThread.scrollHeight;
+});
+
+input.addEventListener("keydown", e => {
+  if (e.key === "Enter") {
+    form.dispatchEvent(new Event("submit"));
+  }
+});
